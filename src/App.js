aa40c48a3aab46082d0b827529b3c5e5b3f71af8
Kranthi babu <kranthibabud1@gmail.com>
2022-02-23 10:32:48 +0530

    adding some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import BirthdayReminder from "./01-birthday-reminder/home";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
+  const Links = () => {
+    return (
+      <center>
+        <div style={{ marginTop: "100px" }}>
+          <ul>
+            <li style={{ listStyle: "none" }}>
+              <Link
+                style={{ textDecoration: "none", fontSize: "25px" }}
+                to={"/birthday"}
+              >
+                Birthday
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </center>
+    );
+  };
   return (
     <div className="App">
-      <BirthdayReminder />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Links />} />
+          <Route path="/birthday" element={<BirthdayReminder />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
